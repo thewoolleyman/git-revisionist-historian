59567097831a19d8b3ec9b5c8fdc601de0a9e053
Chad Woolley <thewoolleyman@gmail.com>
2017-08-14 20:34:40 -0500

    [#150247591] Add error handling to subprocesses

--- src/main/kotlin/com/thewoolleyweb/grh/cmd/run.kt
+++ src/main/kotlin/com/thewoolleyweb/grh/cmd/run.kt
@@ -1,26 +1,38 @@
 package com.thewoolleyweb.grh.cmd
 
 import java.io.File
-import java.io.IOException
 import java.util.concurrent.TimeUnit
 
 fun run(cmd: String, timeout: Long = 5): List<String> {
-  try {
-    val parts = cmd.split("\\s".toRegex())
-    val proc = ProcessBuilder(*parts.toTypedArray())
-      .directory(File("."))
-      .redirectOutput(ProcessBuilder.Redirect.PIPE)
-      .redirectError(ProcessBuilder.Redirect.PIPE)
-      .start()
+  val parts = cmd.split("\\s".toRegex())
+  val proc = ProcessBuilder(*parts.toTypedArray())
+    .directory(File("."))
+    .redirectOutput(ProcessBuilder.Redirect.PIPE)
+    .redirectError(ProcessBuilder.Redirect.PIPE)
+    .start()
 
-    proc.waitFor(timeout, TimeUnit.SECONDS)
-    return proc
-      .inputStream
-      .bufferedReader()
-      .useLines { it.toList() }
-  } catch(e: IOException) {
-    // TODO: return custom exception
-    e.printStackTrace()
-    return arrayListOf("ERROR!")
+  proc.waitFor(timeout, TimeUnit.SECONDS)
+
+  // TODO: How to chronologically interleave stdout and stderr, as well as all other
+  // goals listed in https://github.com/thewoolleyman/process_helper/blob/master/README.md ???
+  // See https://www.pivotaltracker.com/story/show/150247329
+  val stdout = proc
+    .inputStream
+    .bufferedReader()
+    .useLines { it.toList() }
+
+  val stderr = proc
+    .errorStream
+    .bufferedReader()
+    .useLines { it.toList() }
+
+  val exitValue = proc.exitValue()
+  if (exitValue != 0) {
+    val stderrLines = stderr.joinToString("\n")
+    val message = "\n\nError processing command (exit value $exitValue): '$cmd'\n\n" +
+      "Error output:\n$stderrLines\n"
+    throw RuntimeException(message)
   }
+
+  return stdout
 }
--- src/test/kotlin/test/grh/CmdTest.kt
+++ src/test/kotlin/test/grh/CmdTest.kt
@@ -2,14 +2,20 @@ package test.grh
 
 import com.thewoolleyweb.grh.cmd.run
 import io.kotlintest.matchers.shouldBe
+import io.kotlintest.matchers.shouldThrow
 import io.kotlintest.specs.StringSpec
 
 class CmdTest : StringSpec() {
   init {
-    val out = run("echo 'foo'")
     "works" {
+      val out = run("echo 'foo'")
       out shouldBe arrayListOf("'foo'")
     }
 
+    "raises exception on nonzero return code" {
+      shouldThrow<RuntimeException> {
+        run("ls /does_not_exist")
+      }
+    }
   }
 }
