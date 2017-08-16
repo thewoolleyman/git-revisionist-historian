1c1a92b84441c2a728c80392247f25b8dc1673b3
Chad Woolley <thewoolleyman@gmail.com>
2017-08-16 00:02:24 -0600

    [#150285814] add push and some more logging

--- src/main/kotlin/com/thewoolleyweb/grh/processor/process.kt
+++ src/main/kotlin/com/thewoolleyweb/grh/processor/process.kt
@@ -10,13 +10,26 @@ import java.io.File
 import java.io.InputStream
 
 fun process(args: Args): Unit {
+  println("Running with config file ${args.configFile}")
   val grhConfig = load(StringBuilder(readConfigFile(args.configFile)))
   val log = readLog(grhConfig.branchToRevise)
   val plan = createPlan(grhConfig, log)
   val invocations = renderPlan(plan)
+  doLiveRun(invocations)
+}
+
+private fun doLiveRun(invocations: List<String>) {
+  println("Invoking commands:")
   invocations.forEach { invocation ->
-    run(invocation)    
+    runWithLogging(invocation)
   }
+  runWithLogging("git push --tags")
+  println("Finished invoking commands.")
+}
+
+private fun runWithLogging(invocation: String) {
+  println("$ $invocation")
+  print(run(invocation).joinToString("\n"))
 }
 
 private fun readConfigFile(configFile: String): String {
