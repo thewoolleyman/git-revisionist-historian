314562910b89868abd4b2db34c0013dd6869cf8f
Chad Woolley <thewoolleyman@gmail.com>
2017-08-16 17:05:21 -0700

    [#150309899] fix --help

--- src/main/kotlin/com/thewoolleyweb/grh/Main.kt
+++ src/main/kotlin/com/thewoolleyweb/grh/Main.kt
@@ -2,9 +2,23 @@ package com.thewoolleyweb.grh
 
 import com.thewoolleyweb.grh.processor.process
 import com.xenomachina.argparser.ArgParser
+import com.xenomachina.argparser.ShowHelpException
+
 
 fun main(arguments: Array<String>) {
-  val args: Args = Args(ArgParser(arguments))
-  process(args)
+  process(processArgs(arguments))
+}
+
+private fun processArgs(arguments: Array<String>): Args {
+  val parser = ArgParser(arguments)
+  val args: Args = Args(parser)
+
+  // force help to be handled
+  try {
+    parser.force()
+  } catch (e: ShowHelpException) {
+    e.printAndExit("git-revisionist-historian")
+  }
+  return args
 }
 
